51073b174f0e9d5da4d8c77fc8c95a6be2776435
Francis Makokha <francismakokha@hotmail.com>
2015-03-06 22:30:51 +0100

    Suite génération secteur + temps tour

--- Essais/coureur.c
+++ Essais/coureur.c
@@ -2,21 +2,19 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]){
-    int sectorTime=0,totalTime=0,min,hr,i;
+main(){
+    int sectorTime=0,totalTime=0,totalTimeMin,totalTimeSec,i;
     srand(time(NULL));
 
     for(i=0;i<3;i++){
     sectorTime= ((rand()%11)+30); //rand()% x + n | n = min et n +(x-1) = max.
 	//sleep(sectorTime);//Attends le nb de secondes du random avant d'afficher le temps
-        printf("Temps du secteur %d : %d\n",i,sectorTime);
-	totalTime += sectorTime;//Affiche le temps total après chaque secteur
-	if(totalTime >= 60){
-        min= totalTime/60;
-        hr= min/60;
-        printf("Temps total= %d heure(s) %d mins %d s\n",hr,min,sectorTime);
-	}else
-        printf("Temps total= %d\n",totalTime);
+    printf("Temps du secteur %d : %d\n",i+1,sectorTime);
+    totalTime += sectorTime;//Calcule le temps total après chaque tour
     }
-    return 0;
+}
+void tempsTour(int ){
+    totalTimeMin= (totalTime/60);
+	totalTimeSec= (totalTime%60);
+    printf("Temps total pour le tour= %d minute(s) %d secondes \n",totalTimeMin,totalTimeSec);
 }
