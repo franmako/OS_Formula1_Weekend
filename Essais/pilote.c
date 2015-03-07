d7a7a737322451af3b3999d013569a67a4f6352b
Francis Makokha <francismakokha@hotmail.com>
2015-03-07 20:57:19 +0100

    .

--- Essais/pilote.c
+++ Essais/pilote.c
@@ -3,26 +3,34 @@
 #include <unistd.h>
 #include "pilote.h"
 #define NBTOURS 5
+#define NBSECTORS 3
+
+/*typedef struct{
+    int[NBTOURS] tempsTours;
+}Course*/
 
 main(){
-    int i,nbPilote;
+    int i,j,nbPilote,*sectorTime;
     srand(time(NULL));
     for(i=0;i<NBTOURS;i++){
-	printf("|--------------|\n");
-	printf("|*** Tour %d ***|\n",i+1);
-	printf("|--------------|\n");
-        getTempsSecteur();
+        printf("|--------------|\n");
+        printf("|*** Tour %d ***|\n",i);
+        printf("|--------------|\n");
+        sectorTime= getTempsSecteur();
+        //getTempsTour(totalLapTime);
+        for(j=0;j<NBSECTORS;j++)
+            printf("Temps du secteur %d : %d secondes\n",i,*(sectorTime+j));
     }
 }
-void getTempsSecteur(){
-    int i,sectorTime,totalLapTime=0;
+int* getTempsSecteur(){
+    int i,totalLapTime;
+    static int sectorTime[2];
     for(i=0;i<3;i++){
-        sectorTime= ((rand()%11)+30); //rand()% x + n | n = min et n +(x-1) = max.
+        sectorTime[i]= ((rand()%11)+30); //rand()% x + n | n = min et n +(x-1) = max.
         //sleep(sectorTime);//Attends le nb de secondes du random avant d'afficher le temps
-        printf("Temps du secteur %d : %d secondes\n",i+1,sectorTime);
-        totalLapTime += sectorTime;//Calcule le temps total après chaque tour
+        totalLapTime += sectorTime[i];//Calcule le temps total après chaque tour
     }
-    getTempsTour(totalLapTime);
+    return(sectorTime);
 }
 void getTempsTour(int totalLapTime){
     int totalTimeMin,totalTimeSec;
@@ -34,4 +42,5 @@ void getTempsTour(int totalLapTime){
 	    printf("Temps total pour le tour= %d secondes \n\n",totalTimeSec);
 	}
 }
+
 
--- Essais/pilote.h
+++ Essais/pilote.h
@@ -1,2 +1,2 @@
-void getTempsSecteur();
+int *getTempsSecteur();
 void getTempsTour(int);
