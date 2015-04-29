e7b81c112c92fbfc4205836d7cac4f6f5efba176
Francis Makokha <francismakokha@hotmail.com>
2015-04-29 14:42:27 +0200

    Partage du temps secteur

---
+++ serveur.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+#define ERROR -1
+#define SHM_SIZE 1024  /* make it a 1K shared memory segment */
+#define SHM_WRITE 2
